e106b18e1d439d602e95d86991742f1f61f8746c
Lucas Porto <lucaspmv@hotmail.com>
2022-04-20 08:34:09 -0300

    Aplicação finalizada

---
+++ src/screens/Home.tsx
@@ -0,0 +1,108 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform,
+  FlatList,
+} from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greetings, setGreetings] = useState('');
+
+  function handleAddNewSkill(){
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string){
+    setMySkills(oldState => oldState.filter(skill => skill.id !== id));
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreetings('Good morning')
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreetings('Good afternoon');
+    } else {
+      setGreetings('Good night');
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Lucas</Text>
+
+      <Text style={styles.greetings}>
+        {greetings}
+      </Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor={"#555"}
+        onChangeText={setNewSkill}
+      />
+
+      <Button
+        title='Add' 
+        onPress={handleAddNewSkill}  
+      />
+
+      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+      
+      <FlatList 
+        data={mySkills}
+        showsVerticalScrollIndicator={false} 
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <SkillCard 
+            onPress={() => handleRemoveSkill(item.id)}
+            skill={item.name}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  greetings: {
+    color: '#fff'
+  }
+})
